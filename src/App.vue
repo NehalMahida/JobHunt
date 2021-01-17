73660a2326f9325e1cfb56752da75e6210302dce
Nehal Mahida <nehalmahida2144@gmail.com>
2021-01-17 21:47:36 +0530

    Completed

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <h1>JobHunt</h1>
+      </div>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+    <v-main class="grey lighten-5">
+      <Home></Home>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Home from "./components/Home";
+export default {
+  name: "App",
+  components: {
+    //HelloWorld,
+    Home,
+  },
+
+  data: () => ({}),
+};
+</script>
